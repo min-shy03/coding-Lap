e65fd75972ec0a10f0e9acb9b416d5b2ba164a7c
msh <alstngusqkqh@g.yju.ac.kr>
2025-04-15 15:01:35 +0900

    2025-04-15

---
+++ baekjoon_2292.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+num = 1
+
+count = 0
+
+while True :
+    if n <= num:
+        if n == 1 :
+            count += 1
+        break
+
+    num += 6 * count  
+    
+    count += 1
+    
+print(count)
