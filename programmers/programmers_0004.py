bda64e449c1ad1c07b20586af51f690e4dae220c
msh <alstngusqkqh@gmail.com>
2025-05-08 14:39:36 +0900

    프로그래머스 팩토리얼

---
+++ programmers/programmers_0004.py
@@ -0,0 +1,13 @@
+def solution(n):
+    i = 1
+    answer = 1
+    
+    while True :
+        answer *= i
+        if answer > n :
+            i -= 1
+            break
+        i += 1
+    return i
+
+print(solution(7))
