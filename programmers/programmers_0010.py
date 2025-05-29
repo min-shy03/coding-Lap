682e3df7d51e1b8ee6dc6cdf392dc9046d9e6e59
msh <alstngusqkqh@gmail.com>
2025-05-29 14:24:25 +0900

    프로그래머스 공원 산책 주석 추가

--- programmers/programmers_0010.py
+++ programmers/programmers_0010.py
@@ -19,20 +19,25 @@ def solution(park, routes):
                     break
     
     for char in routes :
+        # 방향과 횟수 분리
         loc, count = char.split()
         
+        # 진행 방향에 X가 있는지 확인하기 위한 불린 변수
         check = True
         
         count = int(count)
         if loc == "E" :
+            # 현재 위치 + 이동 횟수가 공원의 전체 크기를 넘어가면 무시 밑의 코드를 실행하려면 무조건 통과해야됨
             if not start[1] + count <= park_width :
                 continue
                 
+            # 현재 가로줄에서 끝까지 이동할때 까지 X가 없는지 체크하고 있으면 check = False 
             for i in range(start[1], start[1] + count+1) :
                 if park[start[0]][i] == "X" :
                     check = False
                     break
-
+            
+            # 위 for문에서 check가 걸리지 않으면 그만큼 이동
             if check :
                 start[1] += count
             
