3a0d69df4f6da0dc158ca3c874e786e9276aa964
msh <alstngusqkqh@gmail.com>
2025-06-05 15:53:02 +0900

    프로그래머스 등수 매기기 코드의 간결화 복습

--- programmers/programmers_0020.py
+++ programmers/programmers_0020.py
@@ -40,3 +40,20 @@ def solution(score):
     return answer
 
 print(solution([[1, 3], [3, 1], [2, 3], [3, 2], [1, 2], [1, 1]])) 
+
+# 더 간결하고 깔끔하게 짤 수 있다. 
+# index() 함수는 무조건 앞의 위치한 값만 불러오는 점을 이용해서 등수가 중복되어도 처리가 가능하다.
+# 여러 함수의 사용법을 숙지하자.
+
+def solution(score):
+    avg_score = []
+    
+    for s in score :
+        avg_score.append(sum(s))
+        
+    ranking = sorted(avg_score, reverse=True)
+    
+    return [ranking.index(sum(i)) + 1 for i in score]
+
+print(solution([[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]))
+    
