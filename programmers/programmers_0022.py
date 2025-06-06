f678f2cba28c63686dde93c17cfbb432a46fb982
msh <alstngusqkqh@gmail.com>
2025-06-06 16:17:30 +0900

    프로그래머스 로그인 성공? 문제 복습 필요 다시 풀어보기

--- programmers/programmers_0022.py
+++ programmers/programmers_0022.py
@@ -15,4 +15,6 @@ def solution(id_pw, db):
     else : 
         return "fail"
 
+# 딕셔너리 혹은 for 문을 써서 깔끔하게 풀 수 있다 나중에 다시 한 번 풀어서 복습하기
+
 print(solution(["meosseugi", "1234"],	[["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]))
