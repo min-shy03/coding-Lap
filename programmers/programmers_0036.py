ff7bf1fdfd3e6d25d4792954f3b375fb2c4ba49d
msh <alstngusqkqh@gmail.com>
2025-06-12 14:14:55 +0900

    프로그래머스 콜라츠 추측

---
+++ programmers/programmers_0036.py
@@ -0,0 +1,23 @@
+# 콜라츠 추측
+
+# 1-1. 입력된 수가 짝수라면 2로 나눕니다. 
+# 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다. 
+# 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다. 
+
+def solution(num):
+    count = 0
+    while num != 1 :
+        if count == 500 :
+            count = -1
+            break
+        
+        if num % 2 :
+            num = num * 3 + 1
+        else :
+            num //= 2
+            
+        count += 1
+            
+    return count
+
+print(solution(16))
