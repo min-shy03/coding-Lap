d2dbf7bb8587a0d430e51cd8bdc21810937f23aa
msh <alstngusqkqh@gmail.com>
2025-07-18 13:46:53 +0900

    백준 11660번 구간 합 구하기 5 재풀이

--- baekjoon/silver/baekjoon_11660.py
+++ baekjoon/silver/baekjoon_11660.py
@@ -1,28 +1,29 @@
 # 구간 합 구하기 5
 
-# DP 개념과 누적, 구간 합 개념을 이용하여 문제를 풀어보자
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().strip().split())
+n,m = map(int, input().split())
 
-# 원본 리스트에 0 인덱스 위치 더 추가
+# 원본 리스트
 A = [[0] * (n + 1)]
 
-# (n+1) * (n+1) 사이즈의 누적 합 리스트 0으로 생성
+# 누적 합 리스트 틀
 D = [[0] * (n + 1) for _ in range(n + 1)]
- 
-for _ in range(n) :
-    A_row = [0] + [int(x) for x in sys.stdin.readline().strip().split()]
-    A.append(A_row)
-    
-# 누적 합 구하기
-for i in range(1, n + 1) :
-    for j in range(1, n + 1) :
-        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
 
+# 원본 리스트 추가
+for _ in range(n) :
+    A_row = [0] + list(map(int, input().split()))
+    A.append(A_row)
+
+# 누적 합 리스트 생성
+for i in range(1, n+1) :
+    for j in range(1, n+1) :
+        D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j]
+
+# 구간 합 구하기
 for _ in range(m) :
-    x1, y1, x2, y2 = map(int, sys.stdin.readline().strip().split())
+    x1, y1, x2, y2 = map(int,input().split())
     
     result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
-    
     print(result)
