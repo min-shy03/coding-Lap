f32fd3d171f96c5767c3ac90addbea12fc8b24f0
msh <alstngusqkqh@gmail.com>
2025-08-09 20:36:59 +0900

    백준 09461번 파도반 수열 처음으로 직접 푼 dp 문제

---
+++ baekjoon/silver/baekjoon_09461.py
@@ -0,0 +1,23 @@
+# 파도반 수열
+
+t = int(input())
+
+dp = [0] * 101
+
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 2
+dp[6] = 3
+dp[7] = 4
+dp[8] = 5
+dp[9] = 7
+dp[10] = 9
+
+for i in range(11, 101) :
+    dp[i] = dp[i-1] + dp[i-5]
+
+for _ in range(t) :
+    n = int(input())
+    print(dp[n])
